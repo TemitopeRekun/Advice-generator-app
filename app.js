feaaf72680f511886275f623eb5ba3f75a13a45f
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-04-24 19:12:36 +0100

    FINISHED THE PROJECT

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const diceBtn = document.querySelector(".button");
+const advice = document.querySelector(".advice");
+const adviceNum = document.querySelector(".advice-num");
+const url = "https://api.adviceslip.com/advice";
 
+async function generate() {
+	const res = await fetch(url);
+	const data = await res.json();
+	let slip = data.slip;
+	let dataId = slip.id;
+	let dataAdvice = slip.advice;
+
+	adviceNum.innerHTML = `advice # ${dataId}`;
+	advice.innerHTML = dataAdvice;
+}
+
+diceBtn.addEventListener("click", generate);
